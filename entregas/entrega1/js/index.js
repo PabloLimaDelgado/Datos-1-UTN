0c3cdb2196f12f140e3729c06fad109cb027af6a
PabloLimaDelgado <limapablomdz@gmail.com>
2023-09-03 14:55:11 -0300

    Segunda entrega V3

--- entregas/entrega1/js/index.js
+++ entregas/entrega1/js/index.js
@@ -1,12 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const paginaMainIndex = document.documentElement;  // Esto seleccionará el elemento <html>
 
-<<<<<<< HEAD
     function pagina100PorCiento(event, page) {
         event.preventDefault();
-=======
-    function pagina100PorCiento(page) {
->>>>>>> b61170951638790cedbda9404bc3fd6ac2a23eeb
         if (page.scrollHeight <= window.innerHeight) {
             page.classList.add('html-init');
         } else {
